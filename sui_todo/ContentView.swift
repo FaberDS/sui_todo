aacac96ae394bd6b763e7fc1d5b1aa57098987ec
Denis Schüle <schueledenis@gmx.at>
2021-10-27 09:47:17 +0200

    init

---
+++ sui_todo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  sui_todo
+//
+//  Created by Denis Schüle on 27.10.21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ sui_todo/sui_todoApp.swift
@@ -0,0 +1,17 @@
+//
+//  sui_todoApp.swift
+//  sui_todo
+//
+//  Created by Denis Schüle on 27.10.21.
+//
+
+import SwiftUI
+
+@main
+struct sui_todoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
